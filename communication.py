3447c83c104135b9d6b967c40e33ff1c8e031231
pedroogaab <plechi_2016@hotmail.com>
2023-06-13 21:12:28 -0300

    update, agora passa por todas as notificações se tomou ou nao, e pega a ultima, se for false envia o email

--- communication.py
+++ communication.py
@@ -33,7 +33,8 @@ def send_email(email_origin, password, email_destiny, subject, message):
         server.starttls()
         server.login(email_origin, password)
         server.send_message(msg)
-        print("\033[1;32;40mE-mail enviado com sucesso! \033[0;0m")
+        print("\033[1;32mE-mail enviado com sucesso!\033[0m")
+
 
 
 email_origin = input("\nE-mail: ")
@@ -47,6 +48,7 @@ response = requests.get(link)
 data = response.json()
 
 for i in data["documents"]:
+
     user = str(i["name"]).split("/")[-1]
     name = str(i["fields"]["Name"]["stringValue"])
     first_name = name.split(" ")[0]
@@ -60,35 +62,23 @@ for i in data["documents"]:
             name_remedio = str(remedio).split("/")[-1]
 
             try:
-                tomou = med["fields"]["log"]["arrayValue"]["values"][0]
-                tomou = tomou["mapValue"]["fields"]["tomou"]["booleanValue"]
+                doses = med['fields']['log']['arrayValue']['values']
+                tomou = doses[-1]["mapValue"]["fields"]["tomou"]["booleanValue"]
+                
 
                 if tomou is False:
                     precisa_tomar.append(name_remedio)
             except:
                 print(f"Erro ao acessar o remedio '{name_remedio}' do {name}")
 
-        local = pytz.timezone("America/Sao_Paulo")
-
-        moment = datetime.now(local)
-        moment = moment.strftime("%D às %H:%M")
-
-        remedio = str(data[0]["name"]).split("/")[-1]
-
-        logs = data[0]["fields"]["Enable"]["booleanValue"]
-
-        # last_pill = data[0]
-        # last_pill = last_pill["fields"]["log"]["mapValue"]["fields"]
-
-        if logs == False:
-            msg.attach(
-                MIMEText(f"Olá {name}, você precisa tomar o seu {remedio}", "plain")
-            )
-            with smtplib.SMTP(smtp_server, smtp_port) as server:
-                server.starttls()
-                server.login(email_origin, password)
-                server.send_message(msg)
-                print("E-mail enviado com sucesso!")
-
-        else:
-            ...
+        if len(precisa_tomar) > 1:
+            subject = f"{first_name}, você precisa tomar seus remédios"
+            message = f"Olá {name}, você precisa tomar os remédios: {', '.join(precisa_tomar)}"
+            send_email(email_origin, password, email_destiny, subject, message)
+        elif len(precisa_tomar) == 1:
+            subject = f"{first_name}, você precisa tomar seu remédio"
+            message = f"Olá {name}, você precisa tomar o remédio: {precisa_tomar[0]}"
+            send_email(email_origin, password, email_destiny, subject, message)
+    else:
+        print(f"Erro ao processar usuário: {name}")
+        continue
